f652df98180d899f87bf07e8ebc269dc2d97c72f
Danny Cardoso <dcardoso066@gmail.com>
2021-05-10 16:58:57 +0200

    exercice completed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,21 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+  def new
+    @letters = []
+    Array.new(9, "").each { @letters << ('A'..'Z').to_a[rand(26)] }
+    return @letters
+  end
+  def score
+    end_time = Time.now
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    user_serialized = open(url).read
+    @user = JSON.parse(user_serialized)
+    @hash = {
+      score: 0,
+      message: "",
+      time: (end_time)
+    }
+  end
 end
