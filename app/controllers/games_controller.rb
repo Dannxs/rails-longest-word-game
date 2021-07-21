1873f007ce67b6fa275fa5f2ac78268a6ed8ee67
Danny Cardoso <dcardoso066@gmail.com>
2021-07-21 12:28:23 +0200

    done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,13 +9,16 @@ class GamesController < ApplicationController
   end
   def score
     end_time = Time.now
+    @attempt = params[:answer]
     url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
     user_serialized = open(url).read
     @user = JSON.parse(user_serialized)
+    
     @hash = {
       score: 0,
       message: "",
       time: (end_time)
     }
   end
+
 end
